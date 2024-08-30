2f84fd55a99cdad401382d45cdd53a2cb1ebf7c1
Rafsan12 <rafsan.web.2022@gmail.com>
2024-08-30 07:47:07 +0600

    catch input and find input value

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const addBtn = document.querySelector("#AddBtn");
 
 addBtn.addEventListener("click", () => {
   //   console.log("AddBtn clicked");
+  const inputValue = document.querySelector("#input");
+
+  const todoValue = inputValue.value;
+  console.log(todoValue);
 });
